cdcb3c2fa1d0a99b12a49b768e984e0e6bde32a1
Collins Muriuki <murerwacollins@gmail.com>
2020-07-07 23:13:24 +0300

    Group configuration values

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@ use std::{env,fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Failed to read file");
+    let contents = fs::read_to_string(config.filename).expect("Failed to read file");
 
     println!("With rext \n{}", contents);
 }
+#[derive(Clone)]
+struct Config {
+    query: String,
+    filename: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
 }

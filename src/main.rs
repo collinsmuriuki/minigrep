169612b8da5b380163b6d49f87a1889e31cd0da2
Collins Muriuki <murerwacollins@gmail.com>
2020-07-08 00:06:32 +0300

    Improve error handling; return Result type for more user friendly errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::{env,fs};
+use std::{env,fs,process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem in parsing arguments, {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -17,13 +20,13 @@ struct Config<'a> {
 }
 
 impl<'a> Config<'a> {
-    pub fn new(args: &[String]) -> Config {
+    pub fn new(args: &[String]) -> Result<Config, &'a str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = &args[1];
         let filename = &args[2];
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
